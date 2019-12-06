283cf798a7bd40d837ce6f4e53c7056f303560b4
Eduard Toma <eduard.daniel.toma@gmail.com>
2019-12-06 11:55:31 +0200

    added first test

---
+++ src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -0,0 +1,36 @@
+package com.hoaxify.hoaxify;
+
+import com.hoaxify.hoaxify.user.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class) // junit4
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class UserControllerTest {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void postUser_whenUserIsValid_receiveOk() {
+        User user = new User();
+        user.setUsername("test-user");
+        user.setDisplayName("test-user");
+        user.setPassword("P4ssword");
+
+        ResponseEntity<Object> responseEntity = testRestTemplate.postForEntity("/api/1.0/users", user, Object.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+}
